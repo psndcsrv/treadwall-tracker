37fe68c04435712bae3366c0086925ec501eb96e
Aaron Unger <aunger@ungerdesign.com>
2018-02-18 16:50:54 -0800

    Add timer and last attempt times

--- app/src/main/java/com/ungerdesign/treadwalltracker/activity/StagingActivity.java
+++ app/src/main/java/com/ungerdesign/treadwalltracker/activity/StagingActivity.java
@@ -4,15 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.ungerdesign.treadwalltracker.R;
 
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
 public class StagingActivity extends AppCompatActivity {
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MM:SS");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staging);
+
+        Intent intent = getIntent();
+        long elapsedTime = intent.getLongExtra(TimerActivity.EXTRA_ELAPSED_TIME, 0);
+
+        TextView lastAttemptLabelView = findViewById(R.id.lastAttemptLabelView);
+        TextView lastAttemptElapsedView = findViewById(R.id.lastAttemptElapsedView);
+        if (elapsedTime > 0) {
+            lastAttemptLabelView.setVisibility(View.VISIBLE);
+            lastAttemptElapsedView.setVisibility(View.VISIBLE);
+
+            lastAttemptElapsedView.setText(formatElapsedTime(elapsedTime));
+        } else {
+            lastAttemptLabelView.setVisibility(View.INVISIBLE);
+            lastAttemptElapsedView.setVisibility(View.INVISIBLE);
+        }
     }
 
     public void startAttempt(View view) {
@@ -23,5 +43,13 @@ public class StagingActivity extends AppCompatActivity {
     public void finishActivity(View view) {
         Intent intent = new Intent(this, ExitInterviewActivity.class);
         startActivity(intent);
+    }
+
+    private String formatElapsedTime(long elapsed) {
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(elapsed);
+        return String.format("%02d:%02d",
+                minutes,
+                TimeUnit.MILLISECONDS.toSeconds(elapsed) - TimeUnit.MINUTES.toSeconds(minutes)
+                );
     }
 }
--- app/src/main/java/com/ungerdesign/treadwalltracker/activity/TimerActivity.java
+++ app/src/main/java/com/ungerdesign/treadwalltracker/activity/TimerActivity.java
@@ -2,12 +2,17 @@ package com.ungerdesign.treadwalltracker.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Chronometer;
 
 import com.ungerdesign.treadwalltracker.R;
 
 public class TimerActivity extends AppCompatActivity {
+    private static final String TAG = TimerActivity.class.getName();
+    public static final String EXTRA_ELAPSED_TIME = "com.ungerdesign.treadwalltracker.ELAPSED_TIME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +20,28 @@ public class TimerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timer);
 
         // TODO Record start time
-        // TODO Start timer
+        long startTime = SystemClock.elapsedRealtime();
+
+        Log.i(TAG, "Starting timer at: " + startTime);
+
+        // Start timer
+        Chronometer chrono = findViewById(R.id.chronometer2);
+        chrono.setBase(startTime);
+        chrono.start();
     }
 
     public void finishAttempt(View view) {
-        // TODO Stop timer
+        // Stop timer
+        Chronometer chrono = findViewById(R.id.chronometer2);
+        long elapsedTime = SystemClock.elapsedRealtime() - chrono.getBase();
+        chrono.stop();
+
+        Log.i(TAG, "Stopping timer with elapsed: " + elapsedTime);
+
         // TODO Record elapsed time
 
         Intent intent = new Intent(this, StagingActivity.class);
+        intent.putExtra(EXTRA_ELAPSED_TIME, elapsedTime);
         startActivity(intent);
     }
 }
